655cad934a3ec5cd16e5d1296c1d68ac882dca39
Oliver Marcell Toth <oliverm.toth@protonmail.ch>
2022-11-24 20:36:17 -0500

    edit minesweeper

--- proj1/minesweeper/minesweeper.py
+++ proj1/minesweeper/minesweeper.py
@@ -105,27 +105,34 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count and self.count != 0:
+            return self.cells
+        return set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
@@ -182,6 +189,20 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
+
+        self.moves_made.add(cell)
+        self.mark_safe(cell)
+
+        sentence = set()
+
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+                if (i, j) == cell or (i, j) in self.safes:
+                    continue
+                elif (i,j) in self.mines:
+                    count -= 1
+
+
         raise NotImplementedError
 
     def make_safe_move(self):
